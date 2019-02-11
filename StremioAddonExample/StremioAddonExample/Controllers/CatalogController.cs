bae9a6c65a79f8d887798353da51a0e843b2701f
Ivo <ivo.paunov@gmail.com>
2019-02-11 23:32:12 +0200

    Add example solution and manifest controller

---
+++ StremioAddonExample/StremioAddonExample/Controllers/CatalogController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StremioAddonExample.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        // GET /catalog
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            // TODO:
+            return "catalog";
+        }
+    }
+}
---
+++ StremioAddonExample/StremioAddonExample/Controllers/ManifestController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StremioAddonExample.Controllers
+{
+
+    [ApiController]
+    public class ManifestController : ControllerBase
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public ManifestController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        [Route("manifest.json")]
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            // TODO: IMemoryCache
+            string contentRootPath = _hostingEnvironment.ContentRootPath;
+            var manifest = System.IO.File.ReadAllText(contentRootPath + "/manifest.json");
+
+            return manifest;
+        }
+    }
+}
