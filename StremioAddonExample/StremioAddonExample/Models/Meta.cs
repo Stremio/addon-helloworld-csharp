a6e8078a83a144692f9dc5b06614de8febd92009
Ivo <ivo.paunov@gmail.com>
2019-02-13 00:36:56 +0200

    Add Meta and Stream models

--- StremioAddonExample/StremioAddonExample/Models/Meta.cs
+++ StremioAddonExample/StremioAddonExample/Models/Meta.cs
@@ -1,14 +1,124 @@
-﻿namespace StremioAddonExample.Models
+﻿using Newtonsoft.Json;
+
+namespace StremioAddonExample.Models
 {
+    public enum Type
+    {
+        movie,
+        series,
+        channel,
+        tv
+    }
+
+    public enum PosterShape
+    {
+        square,
+        regular,
+        landscape
+    }
+
+    public class Video
+    {
+        [JsonProperty("id", Required = Required.Always)]
+        public string Id { get; set; }
+
+        [JsonProperty("title", Required = Required.Always)]
+        public string Title { get; set; }
+
+        [JsonProperty("publishedAt", Required = Required.Always)]
+        public object PublishedAt { get; set; }
+
+        [JsonProperty("thumbnail", NullValueHandling = NullValueHandling.Ignore)]
+        public int Thumbnail { get; set; }
+
+        [JsonProperty("streams", NullValueHandling = NullValueHandling.Ignore)]
+        public Stream[] Streams { get; set; }
+
+        [JsonProperty("available", NullValueHandling = NullValueHandling.Ignore)]
+        public bool Available { get; set; }
+
+        [JsonProperty("season", NullValueHandling = NullValueHandling.Ignore)]
+        public int Season { get; set; }        
+
+        [JsonProperty("episode", NullValueHandling = NullValueHandling.Ignore)]
+        public int Episode { get; set; }
+
+        [JsonProperty("trailer", NullValueHandling = NullValueHandling.Ignore)]
+        public string Trailer { get; set; }
+
+        [JsonProperty("overview", NullValueHandling = NullValueHandling.Ignore)]
+        public string Overview { get; set; }
+    }
+
     public class Meta
     {
-        public string id { get; set; }
-        public string name { get; set; }
-        public int year { get; set; }
-        public string poster { get; set; }
-        public string posterShape { get; set; }
-        public string banner { get; set; }
-        public bool isFree { get; set; }
-        public string type { get; set; }
+        [JsonProperty("id", Required = Required.Always)]
+        public string Id { get; set; }
+
+        [JsonProperty("type", Required = Required.Always)]
+        public string Type { get; set; }
+
+        [JsonProperty("Name", Required = Required.Always)]
+        public string Name { get; set; }
+
+        [JsonProperty("generes", Required = Required.Always)]
+        public string[] Generes { get; set; }
+
+        [JsonProperty("poster", Required = Required.Always)]
+        public string Poster { get; set; }
+
+        [JsonProperty("posterShape", NullValueHandling = NullValueHandling.Ignore)]
+        public string PosterShape { get; set; }
+
+        [JsonProperty("background", NullValueHandling = NullValueHandling.Ignore)]
+        public string Background { get; set; }
+
+        [JsonProperty("logo", NullValueHandling = NullValueHandling.Ignore)]
+        public string Logo { get; set; }
+
+        [JsonProperty("description", NullValueHandling = NullValueHandling.Ignore)]
+        public string Description { get; set; }
+
+        [JsonProperty("releaseInfo", NullValueHandling = NullValueHandling.Ignore)]
+        public string ReleaseInfo { get; set; }
+
+        [JsonProperty("director", NullValueHandling = NullValueHandling.Ignore)]
+        public string[] Director { get; set; }
+
+        [JsonProperty("cast", NullValueHandling = NullValueHandling.Ignore)]
+        public string[] Cast { get; set; }
+
+        [JsonProperty("imdbRating", NullValueHandling = NullValueHandling.Ignore)]
+        public string ImdbRating { get; set; }
+
+        [JsonProperty("dvdRelease", NullValueHandling = NullValueHandling.Ignore)]
+        public string DvdRelease { get; set; }
+
+        [JsonProperty("inTheaters", NullValueHandling = NullValueHandling.Ignore)]
+        public bool InTheaters { get; set; }
+
+        [JsonProperty("videos", NullValueHandling = NullValueHandling.Ignore)]
+        public Video[] Videos { get; set; }
+
+        [JsonProperty("certification", NullValueHandling = NullValueHandling.Ignore)]
+        public string Certification { get; set; }
+
+        [JsonProperty("runtime", NullValueHandling = NullValueHandling.Ignore)]
+        public string Runtime { get; set; }
+
+        [JsonProperty("language", NullValueHandling = NullValueHandling.Ignore)]
+        public string Language { get; set; }
+
+        [JsonProperty("country", NullValueHandling = NullValueHandling.Ignore)]
+        public string Country { get; set; }
+
+        [JsonProperty("awards", NullValueHandling = NullValueHandling.Ignore)]
+        public string Awards { get; set; }
+
+        [JsonProperty("website", NullValueHandling = NullValueHandling.Ignore)]
+        public string Website { get; set; }
+
+        [JsonProperty("isPeered", NullValueHandling = NullValueHandling.Ignore)]
+        public string IsPeered { get; set; }
     }
 }
---
+++ StremioAddonExample/StremioAddonExample/Models/Stream.cs
@@ -0,0 +1,103 @@
+﻿using Newtonsoft.Json;
+
+namespace StremioAddonExample.Models
+{
+    public enum Platform
+    {
+        android,
+        ios
+    }
+
+    public enum Availability
+    {
+        NotAvailable = 0,
+        BearlyAvailable = 1,
+        Ok = 2,
+        HighlyAvailable = 3
+    }
+
+    public class Subtitles
+    {
+        [JsonProperty("url", Required = Required.Always)]
+        public string Url { get; set; }
+
+        [JsonProperty("lang", Required = Required.Always)]
+        public string Lang { get; set; }
+    }
+
+    public class ExternalUri
+    {
+        [JsonProperty("platform", NullValueHandling = NullValueHandling.Ignore)]
+        public string Platform { get; set; }
+
+        [JsonProperty("uri", NullValueHandling = NullValueHandling.Ignore)]
+        public string Uri { get; set; }
+
+        [JsonProperty("appUri", NullValueHandling = NullValueHandling.Ignore)]
+        public string AppUri { get; set; }
+
+    }
+
+    public class Stream
+    {
+        [JsonProperty("url", NullValueHandling = NullValueHandling.Ignore)]
+        public string Url { get; set; }
+
+        [JsonProperty("ytId", NullValueHandling = NullValueHandling.Ignore)]
+        public string YtId { get; set; }
+
+        [JsonProperty("infoHash", NullValueHandling = NullValueHandling.Ignore)]
+        public string InfoHash { get; set; }
+        
+        [JsonProperty("fileIdx", NullValueHandling = NullValueHandling.Ignore)]
+        public uint FileIdx { get; set; }
+
+        [JsonProperty("mapIdx", NullValueHandling = NullValueHandling.Ignore)]
+        public uint MapIdx { get; set; }
+
+        [JsonProperty("externalUrl", NullValueHandling = NullValueHandling.Ignore)]
+        public uint ExternalUrl { get; set; }
+
+        [JsonProperty("externalUris", NullValueHandling = NullValueHandling.Ignore)]
+        public ExternalUri[] ExternalUris { get; set; }
+
+        [JsonProperty("name", NullValueHandling = NullValueHandling.Ignore)]
+        public string Name { get; set; }
+
+        [JsonProperty("title", NullValueHandling = NullValueHandling.Ignore)]
+        public string Title { get; set; }
+
+        [JsonProperty("availability", NullValueHandling = NullValueHandling.Ignore)]
+        public Availability Availability { get; set; }
+
+        [JsonProperty("tag", NullValueHandling = NullValueHandling.Ignore)]
+        public string Tag { get; set; }
+
+        [JsonProperty("isFree", NullValueHandling = NullValueHandling.Ignore)]
+        public bool IsFree { get; set; }
+
+        [JsonProperty("isSubscription", NullValueHandling = NullValueHandling.Ignore)]
+        public bool IsSubscription { get; set; }
+
+        [JsonProperty("isPeered", NullValueHandling = NullValueHandling.Ignore)]
+        public bool IsPeered { get; set; }
+
+        [JsonProperty("subtitles", NullValueHandling = NullValueHandling.Ignore)]
+        public Subtitles[] Subtitles { get; set; }
+
+        [JsonProperty("subtitlesExclusive", NullValueHandling = NullValueHandling.Ignore)]
+        public bool SubtitlesExclusive { get; set; }
+
+        [JsonProperty("live", NullValueHandling = NullValueHandling.Ignore)]
+        public bool Live { get; set; }
+
+        [JsonProperty("repeat", NullValueHandling = NullValueHandling.Ignore)]
+        public bool Repeat { get; set; }
+
+        [JsonProperty("geos", NullValueHandling = NullValueHandling.Ignore)]
+        public string[] Geos { get; set; }
+
+        [JsonProperty("meta", NullValueHandling = NullValueHandling.Ignore)]
+        public object Meta { get; set; }
+    }
+}
